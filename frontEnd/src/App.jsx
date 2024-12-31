df757016f4d8bb400a6386da90ea295010239e94
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-12-31 20:16:44 +0330

    (front-end): Fix: The react query congigs is added.

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,13 +1,20 @@
-
-import { BrowserRouter } from "react-router-dom";
-
 import Router from "./router/Router";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import defaultOptions from "./configs/reactQuery";
 
 function App() {
+  const queryClient = new QueryClient({ defaultOptions });
+
   return (
-    <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
         <Router />
-    </BrowserRouter>
+      </BrowserRouter>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
