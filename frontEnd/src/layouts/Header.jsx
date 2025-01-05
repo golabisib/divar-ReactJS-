20e30bf18c9a3363723814450b6ce2f74bb58df1
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2025-01-05 22:58:45 +0330

    (front-end): Fix: The admin & dashboard button in layout is added.

--- frontEnd/src/layouts/Header.jsx
+++ frontEnd/src/layouts/Header.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 
 import styles from "./Header.module.css";
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "src/services/user";
 
 function Header() {
+  const { data } = useQuery(["profile"], getProfile);
   return (
     <header className={styles.header}>
       <div>
         <Link to="/">
-            <img src="divar.svg" className={styles.logo} />
+          <img src="divar.svg" className={styles.logo} />
         </Link>
         <span>
           <img src="location.svg" />
@@ -15,12 +18,21 @@ function Header() {
         </span>
       </div>
       <div>
-        <Link to="/auth">
-          <span>
-            <img src="profile.svg" />
-            <p>چیزای من</p>
-          </span>
-        </Link>
+        {data && data.data.role === "ADMIN" ? (
+          <Link to="/admin">
+            <span>
+              <img src="profile.svg" />
+              <p>پنل ادمین</p>
+            </span>
+          </Link>
+        ) : (
+          <Link to="/auth">
+            <span>
+              <img src="profile.svg" />
+              <p>چیزای من</p>
+            </span>
+          </Link>
+        )}
         <Link to="/dashboard" className={styles.button}>
           ثبت چیز
         </Link>
