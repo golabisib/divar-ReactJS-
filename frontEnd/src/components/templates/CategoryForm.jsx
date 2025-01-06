955e6b5ac9e3028fd7c742042b8ea2cd0a9690af
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2025-01-06 12:39:49 +0330

    (front-end): Feature: The posting a new Category is added.

--- frontEnd/src/components/templates/CategoryForm.jsx
+++ frontEnd/src/components/templates/CategoryForm.jsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
 
-import styles from "./CategoryForm.module.css"
+import { addCategory } from "src/services/admin";
+
+import styles from "./CategoryForm.module.css";
+
 
 function CategoryForm() {
   const [form, setForm] = useState({ name: "", slug: "", icon: "" });
+
+  const {mutate, isLoading, error, data} = useMutation(addCategory)
+  console.log({isLoading, error, data})
 
   const changeHandler = (event) => {
     setForm({ ...form, [event.target.name]: event.target.value });
@@ -11,23 +18,29 @@ function CategoryForm() {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    console.log(form);
+    if(!form.name || !form.slug || !form.icon) return;
+    mutate(form)
   };
 
   return (
-    <form onChange={changeHandler} onSubmit={submitHandler} className={styles.form}>
+    <form
+      onChange={changeHandler}
+      onSubmit={submitHandler}
+      className={styles.form}
+    >
       <h3>دسته بندی جدید</h3>
-      {/* <p></p> */}
+      {!!error && <p>مشکلی پیش آمده است</p>}
+      {data?.status === 201 && <p>دسته بندی با موفقیت اضافه شد</p>}
       <label htmlFor="name">نام دسته بندی</label>
-      <input type="text" name="name" id="name" />
+      <input type="text" name="name" id="name" autoComplete="off" />
 
       <label htmlFor="slug">اسلاگ</label>
-      <input type="text" name="slug" id="slug" />
+      <input type="text" name="slug" id="slug" autoComplete="off" />
 
       <label htmlFor="icon">نام آیکون</label>
-      <input type="text" name="icon" id="icon" />
+      <input type="text" name="icon" id="icon" autoComplete="off" />
 
-      <button type="submit">ایجاد دسته بندی</button>
+      <button type="submit" disabled={isLoading}>ایجاد دسته بندی</button>
     </form>
   );
 }
