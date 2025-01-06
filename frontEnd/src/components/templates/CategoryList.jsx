1f3f7c74a652814eeadb80f49e3c4a004665d065
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2025-01-06 14:46:57 +0330

    (front-end): Feature: The delete Button and style of CategoryForm is added.

--- frontEnd/src/components/templates/CategoryList.jsx
+++ frontEnd/src/components/templates/CategoryList.jsx
@@ -1,16 +1,41 @@
-import { useQuery } from "@tanstack/react-query";
-import { getCategory } from "src/services/admin";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getCategory, deleteCategory } from "src/services/admin";
+
 import Loader from "../modules/Loader";
 
-function CategoryList() {
-  const { data, isLoading } = useQuery(["get-categories"], getCategory);
-  console.log(data, isLoading);
+import styles from "./CategoryList.module.css";
 
-  return <div>{isLoading ? <Loader /> : data.data.map((i) => <div key={i._id}>
-    <img src={`${i.icon}.svg`} />
-    <h5>name : {i.name}</h5>
-    <p>slug : {i.slug}</p>
-  </div>)}</div>;
+function CategoryList() {
+  const queryClient = useQueryClient();
+  const { data, isLoading } = useQuery(["get-categories"], getCategory);
+
+  const mutation = useMutation(deleteCategory, {
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries(["get-categories"]);
+    },
+  });
+
+  const deleteHandler = (id) => {
+    mutation.mutate(id);
+  };
+
+  return (
+    <div className={styles.list}>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        data.data.map((i) => (
+          <div key={i._id}>
+            <img src={`${i.icon}.svg`} alt={i.name} />
+            <h5>{i.name}</h5>
+            <p>slug : {i.slug}</p>
+            <button onClick={() => deleteHandler(i._id)}>حذف</button>
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
 
 export default CategoryList;
