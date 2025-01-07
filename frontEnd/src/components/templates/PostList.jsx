4611fc3d62368ba7a996ebdad7c20c8984655b98
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2025-01-07 23:44:11 +0330

    (front-end): Feature: The List of each user getting and add to dashboard page.

---
+++ frontEnd/src/components/templates/PostList.jsx
@@ -0,0 +1,52 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { deletePost, getPosts } from "src/services/user";
+
+import Loader from "../modules/Loader";
+
+function PostList() {
+  const queryClient = useQueryClient();
+  const { data, isFetching } = useQuery(["my-post-list"], getPosts);
+  console.log(data);
+
+  const mutation = useMutation(deletePost, {
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries(["my-post-list"]);
+    },
+  });
+
+  const deleteHandler = (id) => {
+    mutation.mutate(id);
+  };
+  return (
+    <div>
+      {
+        <div>
+          {isFetching ? (
+            <Loader />
+          ) : (
+            <>
+              <h3>آگهی های من</h3>
+              {data.data.posts.map((post) => (
+                <div key={post._id}>
+                    <img src={`${import.meta.env.VITE_BASE_URL}${post.images[0]}` }alt={post.name}  />
+                    <div>
+                        <p>{post.options.title}</p>
+                        <span>{post.options.content}</span>
+                    </div>
+                    <div>
+                        <p>{post.createdAt}</p>
+                        <span>{post.amount}  تومان </span>
+                    </div>
+                  <button onClick={() => deleteHandler(post._id)}>حذف</button>
+                </div>
+              ))}
+            </>
+          )}
+        </div>
+      }
+    </div>
+  );
+}
+
+export default PostList;
