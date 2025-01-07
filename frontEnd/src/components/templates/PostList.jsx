c6edeb29287249b675b1d6f081a05527d22a4adb
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2025-01-08 01:01:27 +0330

    (front-end): Feature: The styles of form is added.

--- frontEnd/src/components/templates/PostList.jsx
+++ frontEnd/src/components/templates/PostList.jsx
@@ -4,6 +4,8 @@ import { deletePost, getPosts } from "src/services/user";
 import Loader from "../modules/Loader";
 import { sp } from "src/utils/numbers";
 
+import styles from "./PostList.module.css"
+
 function PostList() {
   const queryClient = useQueryClient();
   const { data, isFetching } = useQuery(["my-post-list"], getPosts);
@@ -22,14 +24,14 @@ function PostList() {
   return (
     <div>
       {
-        <div>
+        <div className={styles.list}>
           {isFetching ? (
             <Loader />
           ) : (
             <>
               <h3>آگهی های من</h3>
               {data.data.posts.map((post) => (
-                <div key={post._id}>
+                <div key={post._id} className={styles.post}>
                   <img
                     src={`${import.meta.env.VITE_BASE_URL}${post.images[0]}`}
                     alt={post.name}
@@ -38,7 +40,7 @@ function PostList() {
                     <p>{post.options.title}</p>
                     <span>{post.options.content}</span>
                   </div>
-                  <div>
+                  <div className={styles.price}>
                     <p>
                       {new Date(post.createdAt).toLocaleDateString("fa-IR")}
                     </p>
